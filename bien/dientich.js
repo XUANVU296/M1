cd2c7d52a9dfa158c74cf983fdd83b69d9ecb7dd
vu nguyen <vutrinhsat2023@gmail.com>
2023-06-09 16:19:21 +0700

    dientich

---
+++ bien/dientich.js
@@ -0,0 +1,5 @@
+<script>
+let bankinh = parseFloat(prompt("nhập bán kính hình tròn"));
+let dientich = Math.PI * Math.pow(bankinh, 2);
+document.write("diện tích hình tròn là: "+ dientich);
+</script>
